d30d15a80dfe3fb6f10be961814f70e578fe24cd
somteacodes <stomteacodes@gmail.com>
2021-10-02 17:31:50 +0100

    added pwa support

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
 import '@/assets/css/style.css'
+import './registerServiceWorker'
 
 createApp(App).use(store).use(router).mount('#app')
